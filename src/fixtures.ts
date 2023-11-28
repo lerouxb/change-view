493ce8d3df5312708a0f0232cfa4e8fe2ffc2a10
Le Roux Bodenstein <lerouxb@gmail.com>
2023-11-28 08:52:16 +0000

    airbnb fixture

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import { Buffer } from 'buffer';
 import {
+  EJSON,
   BSONRegExp,
   Binary,
   Code,
@@ -117,6 +118,136 @@ const smallChangeDoc: Document = _.clone(allTypesDoc);
 
 smallChangeDoc.string = 'oh no!';
 
+const airbnb = EJSON.deserialize({
+  "_id": {
+    "$oid": "65648c68cf3ba12a2fcb9c1e"
+  },
+  "id": 13913,
+  "listing_url": "https://www.airbnb.com/rooms/13913",
+  "scrape_id": {
+    "$numberLong": "20220910194334"
+  },
+  "last_scraped": {
+    "$date": "2022-09-11T00:00:00.000Z"
+  },
+  "source": "city scrape",
+  "name": "Holiday London DB Room Let-on going",
+  "description": "My bright double bedroom with a large window has a relaxed feeling! It comfortably fits one or two and is centrally located just two blocks from Finsbury Park. Enjoy great restaurants in the area and easy access to easy transport tubes, trains and buses. Babies and children of all ages are welcome.<br /><br /><b>The space</b><br />Hello Everyone,<br /><br />I'm offering my lovely double bedroom in Finsbury Park area (zone 2) for let in a shared apartment. <br />You will share the apartment with me and it is fully furnished with a self catering kitchen. Two people can easily sleep well as the room has a queen size bed. I also have a travel cot for a baby for guest with small children. <br /><br />I will require a deposit up front as a security gesture on both our parts and will be given back to you when you return the keys. <br /><br />I trust anyone who will be responding to this add would treat my home with care and respect . <br /><br />Best Wishes <br /><br />Alina<br /><br /><b>Gue",
+  "neighborhood_overview": "Finsbury Park is a friendly melting pot community composed of Turkish, French, Spanish, Middle Eastern, Irish and English families. <br />We have a wonderful variety of international restaurants directly under us on Stroud Green Road. And there are many shops and large Tescos supermarket right next door. <br /><br />But you can also venture up to Crouch End and along Greens Lanes where there will endless choice of Turkish and Middle Eastern cuisines.s",
+  "picture_url": "https://a0.muscache.com/pictures/miso/Hosting-13913/original/d755aa6d-cebb-4464-80be-2722c921e8d5.jpeg",
+  "host_id": 54730,
+  "host_url": "https://www.airbnb.com/users/show/54730",
+  "host_name": "Alina",
+  "host_since": {
+    "$date": "2009-11-16T00:00:00.000Z"
+  },
+  "host_location": "London, United Kingdom",
+  "host_about": "I am a Multi-Media Visual Artist and Creative Practitioner in Education. I live in  London England with a Greek/Canadian origins and work internationally. \r\n\r\nI love everything there is to be enjoyed in life and travel is on top of my list!",
+  "host_response_time": "within a day",
+  "host_response_rate": "80%",
+  "host_acceptance_rate": "70%",
+  "host_is_superhost": false,
+  "host_thumbnail_url": "https://a0.muscache.com/im/users/54730/profile_pic/1327774386/original.jpg?aki_policy=profile_small",
+  "host_picture_url": "https://a0.muscache.com/im/users/54730/profile_pic/1327774386/original.jpg?aki_policy=profile_x_medium",
+  "host_neighbourhood": "LB of Islington",
+  "host_listings_count": 3,
+  "host_total_listings_count": 4,
+  "host_verifications": "['email', 'phone']",
+  "host_has_profile_pic": true,
+  "host_identity_verified": true,
+  "neighbourhood": "Islington, Greater London, United Kingdom",
+  "neighbourhood_cleansed": "Islington",
+  "latitude": 51.56861,
+  "longitude": -0.1127,
+  "property_type": "Private room in rental unit",
+  "room_type": "Private room",
+  "accommodates": 1,
+  "bathrooms_text": "1 shared bath",
+  "bedrooms": 1,
+  "beds": 1,
+  "amenities": [
+    "Extra pillows and blankets",
+    "Oven",
+    "Fire extinguisher",
+    "Hair dryer",
+    "Hangers",
+    "Crib",
+    "Dishes and silverware",
+    "Luggage dropoff allowed",
+    "Essentials",
+    "Outlet covers",
+    "Patio or balcony",
+    "Shampoo",
+    "Free parking on premises",
+    "TV with standard cable",
+    "Free street parking",
+    "Cooking basics",
+    "Bed linens",
+    "Babysitter recommendations",
+    "Carbon monoxide alarm",
+    "Bathtub",
+    "Heating",
+    "Wifi",
+    "Building staff",
+    "Children’s books and toys",
+    "Coffee maker",
+    "Long term stays allowed",
+    "Pack ’n play/Travel crib",
+    "Refrigerator",
+    "Room-darkening shades",
+    "Iron",
+    "Kitchen",
+    "Stove",
+    "Lock on bedroom door",
+    "Hot water",
+    "Washer",
+    "Paid parking off premises",
+    "Children’s dinnerware",
+    "Smoke alarm",
+    "Ethernet connection",
+    "Dryer",
+    "Cable TV"
+  ],
+  "price": "$50.00",
+  "minimum_nights": 1,
+  "maximum_nights": 29,
+  "minimum_minimum_nights": 1,
+  "maximum_minimum_nights": 1,
+  "minimum_maximum_nights": 29,
+  "maximum_maximum_nights": 29,
+  "minimum_nights_avg_ntm": 1,
+  "maximum_nights_avg_ntm": 29,
+  "has_availability": true,
+  "availability_30": 17,
+  "availability_60": 38,
+  "availability_90": 68,
+  "availability_365": 343,
+  "calendar_last_scraped": {
+    "$date": "2022-09-11T00:00:00.000Z"
+  },
+  "number_of_reviews": 30,
+  "number_of_reviews_ltm": 9,
+  "number_of_reviews_l30d": 0,
+  "first_review": {
+    "$date": "2010-08-18T00:00:00.000Z"
+  },
+  "last_review": {
+    "$date": "2022-07-15T00:00:00.000Z"
+  },
+  "review_scores_rating": 4.9,
+  "review_scores_accuracy": 4.82,
+  "review_scores_cleanliness": 4.89,
+  "review_scores_checkin": 4.86,
+  "review_scores_communication": 4.93,
+  "review_scores_location": 4.75,
+  "review_scores_value": 4.82,
+  "instant_bookable": false,
+  "calculated_host_listings_count": 2,
+  "calculated_host_listings_count_entire_homes": 1,
+  "calculated_host_listings_count_private_rooms": 1,
+  "calculated_host_listings_count_shared_rooms": 0,
+  "reviews_per_month": 0.2
+}, { relaxed: false });
 
 export const fixtures: Fixture[] = [
   {
@@ -253,6 +384,10 @@ export const fixtures: Fixture[] = [
     name: 'remove number next to object in array',
     before: { foo: [0, { bar: 'baz' }]},
     after: { foo: [{ bar: 'baz' }]},
+  }, {
+    name: 'airbnb',
+    before: airbnb,
+    after: _.clone(airbnb)
   }
 ];
 
